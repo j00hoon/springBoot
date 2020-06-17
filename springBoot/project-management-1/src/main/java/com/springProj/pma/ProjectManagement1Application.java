4a4097612120fc1ad1a5e859931d0b882064c4c7
j00hoon <j00hoon1101@gmail.com>
2020-06-16 22:52:04 -0500

    New Project Management-1 06/16/2020

---
+++ springBoot/project-management-1/src/main/java/com/springProj/pma/ProjectManagement1Application.java
@@ -0,0 +1,13 @@
+package com.springProj.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagement1Application.class, args);
+	}
+
+}
---
+++ springBoot/project-management-1/src/test/java/com/springProj/pma/ProjectManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springProj.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
