cf8c958b649f463c29b68200e663c97d154310cb
j00hoon <j00hoon1101@gmail.com>
2020-09-01 22:38:05 -0500

    new Spring MVC

---
+++ SpringMVC_Sep2020/project-management/src/test/java/com/sb/pma/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
